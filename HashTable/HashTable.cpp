df0a9e0439a275409a0f6cb6ffc468d09fc51ecc
Ted-Denisenko <91020107+Ted-Denisenko@users.noreply.github.com>
2022-08-02 17:51:42 +0700

    Создал функции:  - создания таблицы  - освобождения элемента таблицы из памяти  - освобождения таблицы из памяти

--- HashTable/HashTable.cpp
+++ HashTable/HashTable.cpp
@@ -3,11 +3,18 @@
 #include <string>
 
 typedef struct HT_item HT_item;
-
 struct HT_item
 {
 	char* key;
 	char* value;
+};
+
+typedef struct HashTable HashTable;
+struct HashTable
+{
+	HT_item** items;
+	int size;
+	int count;
 };
 
 unsigned long hashFunction(char* str)
@@ -21,7 +28,9 @@ unsigned long hashFunction(char* str)
 // create item with key-value pair in HT
 HT_item* createItem(char* key, char* value)
 {
-	HT_item* item = (HT_item*)malloc(sizeof(HT_item));
+	HT_item* item = new HT_item;
+
+	// TODO: стоит ли менять на 'new'?
 	item->key = (char*)malloc(strlen(key) + 1);
 	item->value = (char*)malloc(strlen(value) + 1);
 
@@ -31,7 +40,41 @@ HT_item* createItem(char* key, char* value)
 	return item;
 }
 
+HashTable* create_table(int size)
+{
+	HashTable* table = new HashTable;
+	table->size = size;
+	table->count = 0;
+	table->items = new HT_item*;
 
+	for (int i; i < table->size; i++)
+		table->items[i] = NULL;
+
+	return table;
+}
+
+void freeItem(HT_item* item)
+{
+	// потому что malloc
+	free(item->key);
+	free(item->value);
+
+	// потому что new
+	delete item;
+}
+
+void freeTable(HashTable* table)
+{
+	for (int i; i < table->size; i++)
+	{
+		HT_item* item = table->items[i];
+		if (item != NULL)
+			freeItem(item);
+	}
+
+	delete table->items;
+	delete table;
+}
 
 int main()
 {
